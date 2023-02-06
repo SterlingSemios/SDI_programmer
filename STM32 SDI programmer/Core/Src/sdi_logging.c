0edee51280bdf418ad3c5b68649b340872fd397c
Sterling Smith <ssmith@semios.com>
2023-02-06 13:07:00 -0800

    [EMBED-3494] SDI programmer driver from STM8 to STM32 -Initial STM32 project commit -New STM32 project created for SDI programmer -USARTs initialized and communication achieved

---
+++ STM32 SDI programmer/Core/Src/sdi_logging.c
@@ -0,0 +1,12 @@
+#include "sdi_logging.h"
+#include <string.h>
+
+extern UART_HandleTypeDef huart2;
+
+void printLog(char* message)
+{
+    int msgLen = strlen(message);
+    char logMessage[msgLen + 3];
+    sprintf(logMessage, "%s\r\n", message);
+    HAL_UART_Transmit(&huart2, (uint8_t *)logMessage, strlen(logMessage), 100);
+}
